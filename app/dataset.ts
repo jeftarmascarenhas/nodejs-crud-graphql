e21aa0684569f104160d5cc8f91dda88847bd6b3
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2022-09-09 22:54:16 -0300

    feat: create dataset

---
+++ app/dataset.ts
@@ -0,0 +1,12 @@
+export type User = {
+  id: number;
+  name: string;
+};
+
+const users: User[] = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Rue" },
+  { id: 3, name: "Lexi" },
+];
+
+export default users;
